03237f46cb6598d8d12d765fa881bf3d09328a3f
MateusDeFreitasRosa <mateusfreitasrosa@hotmail.com>
2021-06-24 14:40:04 -0300

    init project

---
+++ setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 24 14:29:57 2021
+
+@author: mateu
+"""
+from setuptools import setup
+
+setup(
+    name='Package_Python',
+    packages=['Package_Python'],
+    description='My first package_test_py',
+    version='0.1',
+    url='https://github.com/MateusDeFreitasRosa/package_python',
+    author='Linode',
+    author_email='Mateus de Freitas Rosa',
+    keywords=['pip']
+)
