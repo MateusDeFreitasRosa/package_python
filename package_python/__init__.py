df6f9b5c35fb5e48b8469f4940a3e45a678a05a9
MateusDeFreitasRosa <mateusfreitasrosa@hotmail.com>
2021-06-24 22:19:12 -0300

    save

--- package_python/__init__.py
+++ package_python/__init__.py
@@ -4,24 +4,3 @@ Created on Thu Jun 24 14:34:50 2021
 
 @author: mateu
 """
-# Binary Search
-import math
-
-def binary_search(element, list_elements):
-    list_elements = sorted(list_elements)
-    left = 0
-    right = len(list_elements)
-    itarations = 1
-    while True:
-        middle = math.floor((left+right)/2)
-        if element == list_elements[middle]:
-            return {'iterations': itarations, 'position_element': middle}
-        elif element > list_elements[middle]:
-            left = middle
-        elif element < list_elements[middle]:
-            right = middle
-            
-        if right == left:
-            return {'iterations': itarations, 'position_element': middle}
-        
-        itarations+=1
