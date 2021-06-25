863c2c6006f3dc826f16836ca9cb4c9dcabf2598
MateusDeFreitasRosa <mateusfreitasrosa@hotmail.com>
2021-06-24 22:58:27 -0300

    save

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ from setuptools import setup
 
 setup(
     name='package_python',
-    packages=['package_python'],
+    packages=['./src/package_python'],
     description='My first package_test_py',
     version='1.0.0',
     url='https://github.com/MateusDeFreitasRosa/package_python',
     author='Mateus de Freitas Rosa',
-    author_email='mateusfreitasrosa@hotmail.com',
+    author_email='mateusfreitasrosa@hotmail.com',   
     keywords=['pip'],
     license='MIT'
 )
